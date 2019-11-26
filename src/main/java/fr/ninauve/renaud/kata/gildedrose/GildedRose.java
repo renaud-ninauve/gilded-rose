94bef081754c4094d6df019fdf5e436600c16004
renaud-ninauve <renaud.ninauve.92@gmail.com>
2019-11-26 09:38:33 +0100

    one if per item type

--- src/main/java/fr/ninauve/renaud/kata/gildedrose/GildedRose.java
+++ src/main/java/fr/ninauve/renaud/kata/gildedrose/GildedRose.java
@@ -9,39 +9,26 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie")
-                    || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].name.equals("Aged Brie")) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
                 }
-                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].sellIn < 11 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-
-                    if (items[i].sellIn < 6 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            } else {
-                if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                } else {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            }
-
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
                 items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].name.equals("Aged Brie")) {
                 if (items[i].sellIn < 0 && items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
                 }
             } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+                if (items[i].sellIn < 11 && items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+
+                if (items[i].sellIn < 6 && items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+                items[i].sellIn = items[i].sellIn - 1;
                 if (items[i].sellIn < 0) {
                     items[i].quality = items[i].quality - items[i].quality;
                 }
@@ -49,8 +36,14 @@ class GildedRose {
                 if (items[i].sellIn < 0 && items[i].quality > 0) {
                     continue;
                 }
-            } else if (items[i].sellIn < 0 && items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
+            } else {
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
+                items[i].sellIn = items[i].sellIn - 1;
+                if (items[i].sellIn < 0 && items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
             }
         }
     }
