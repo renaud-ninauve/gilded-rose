f7f804abd9217b251013b6333132ab44cfe356aa
renaud-ninauve <renaud.ninauve.92@gmail.com>
2019-11-26 14:20:16 +0100

    move updateQuality to Item

--- src/main/java/fr/ninauve/renaud/kata/gildedrose/GildedRose.java
+++ src/main/java/fr/ninauve/renaud/kata/gildedrose/GildedRose.java
@@ -8,43 +8,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals("Aged Brie")) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-                if (items[i].sellIn < 0 && items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-            } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-                if (items[i].sellIn < 11 && items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-
-                if (items[i].sellIn < 6 && items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-                if (items[i].sellIn < 0) {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (items[i].sellIn < 0 && items[i].quality > 0) {
-                    continue;
-                }
-            } else {
-                if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-                if (items[i].sellIn < 0 && items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
+        for (Item item: items) {
+            item.updateQuality();
         }
     }
 }
--- src/main/java/fr/ninauve/renaud/kata/gildedrose/Item.java
+++ src/main/java/fr/ninauve/renaud/kata/gildedrose/Item.java
@@ -2,6 +2,10 @@ package fr.ninauve.renaud.kata.gildedrose;
 
 public class Item {
 
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
     public String name;
 
     public int sellIn;
@@ -14,6 +18,51 @@ public class Item {
         this.quality = quality;
     }
 
+    public void updateQuality() {
+
+        switch (name) {
+            case AGED_BRIE:
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0 && quality < 50) {
+                    quality = quality + 1;
+                }
+                break;
+            case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+                if (sellIn < 11 && quality < 50) {
+                    quality = quality + 1;
+                }
+
+                if (sellIn < 6 && quality < 50) {
+                    quality = quality + 1;
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+                break;
+            case SULFURAS_HAND_OF_RAGNAROS:
+                if (sellIn < 0 && quality > 0) {
+                    return;
+                }
+                break;
+            default:
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+                sellIn = sellIn - 1;
+                if (sellIn < 0 && quality > 0) {
+                    quality = quality - 1;
+                }
+                break;
+        }
+    }
+    
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
